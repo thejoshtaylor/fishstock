af6b3cc9a8fa388ad5efa2197eabf2d6e7af1b7c
Joshua Taylor <theofficialjoshuataylor@gmail.com>
2025-02-21 07:10:19 +0000

    Added more moves to main to test bad en passant

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,33 @@ int main() {
     Util::printBoard(&board);
     board.move((Board::Position){6, 3}, (Board::Position){4, 3});
     Util::printBoard(&board);
+
+    // Take en passant
     board.move((Board::Position){4, 2}, (Board::Position){5, 3});
     Util::printBoard(&board);
 
+    // Try non en passantable
+    board.move((Board::Position){6, 7}, (Board::Position){5, 7});
+    Util::printBoard(&board);
+    board.move((Board::Position){1, 5}, (Board::Position){3, 5});
+    Util::printBoard(&board);
+    board.move((Board::Position){5, 7}, (Board::Position){4, 7});
+    Util::printBoard(&board);
+    board.move((Board::Position){3, 5}, (Board::Position){4, 5});
+    Util::printBoard(&board);
+    board.move((Board::Position){6, 6}, (Board::Position){4, 6});
+    Util::printBoard(&board);
+    board.move((Board::Position){1, 0}, (Board::Position){2, 0});
+    Util::printBoard(&board);
+    board.move((Board::Position){6, 0}, (Board::Position){5, 0});
+    Util::printBoard(&board);
 
+    // Try en passantable that shouldn't work
+    try {
+        board.move((Board::Position){4, 5}, (Board::Position){5, 6});
+    } catch (std::invalid_argument &e) {
+        std::cout << "CORRECTLY Caught exception: " << e.what() << std::endl;
+    }
 
     return 0;
 }
